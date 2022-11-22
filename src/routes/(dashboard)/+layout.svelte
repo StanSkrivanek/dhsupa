591c53a40c79357effe207ed2f4446617fc109f8
Stan Skrivanek <s.skrivanek@icloud.com>
2022-11-22 22:23:31 +0000

    CSS: reponsive invoice Grid

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -4,7 +4,7 @@
 
 <div class="grid min-h-screen grid-cols-12 bg-whisper md:gap-x-16">
   <Navbar />
-  <main class="col-span-12 md:col-span-8 md:pt-10">
+  <main class="col-span-12 md:col-span-8 md:pt-10 px-4 pt-4">
     <slot><!-- optional fallback --></slot>
   </main>
 </div>
--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -27,7 +27,7 @@
 <!-- list of invoices -->
 <div>
   <!-- header -->
-  <div class="table-header invoice-table text-daisyBush lg:grid lg:p-6">
+  <div class="table-header invoice-table hidden text-daisyBush lg:grid lg:px-6">
     <h3>Status</h3>
     <h3>Due Date</h3>
     <h3>ID #</h3>
@@ -39,17 +39,17 @@
 
   <!-- list of invioces -->
   <div
-    class="invoice-table mb-3 grid grid-cols-invoiceTableMobile items-center gap-x-4 rounded-lg bg-white px-4 py-6 shadow-tableRow lg:grid-cols-invoiceTable lg:p-6"
+    class="invoice-table invoice-row mb-3 grid grid-cols-invoiceTableMobile items-center gap-x-4 rounded-lg bg-white px-4 py-3 shadow-tableRow lg:grid-cols-invoiceTable lg:py-6"
   >
     <div class="status"><Tag className="ml-auto lg:ml-0" label="draft" /></div>
     <div class="dueDate text-sm lg:text-lg">12/12/20225</div>
     <div class="invoiceNr text-sm lg:text-lg">55555</div>
-    <div class="client text-base font-bold lg:text-lg">fds</div>
+    <div class="clientName text-base font-bold lg:text-lg">fds</div>
     <div class="amount text-right font-mono text-sm lg:text-lg">$100.00</div>
-    <div class="vievBtn hidden lg:block ">
+    <div class=" viewBtn hidden text-sm lg:block ">
       <a href="/" class=" text-pastelPurple hover:text-daisyBush"><Eye /></a>
     </div>
-    <div class="moreBtn hidden lg:block">
+    <div class="moreBtn hidden text-sm lg:block">
       <button class="center text-pastelPurple hover:text-daisyBush"><ThreeDots /></button>
     </div>
   </div>
@@ -64,12 +64,12 @@
   .invoice-row {
     grid-template-areas:
       'invoiceNr invoiceNr'
-      'client amount'
+      'clientName amount'
       'dueDate status';
   }
   @media (min-width: 1024px) {
     .invoice-row {
-      grid-template-areas: 'status dueDate invoiceNr client amount viewBtn moreBtn';
+      grid-template-areas: 'status dueDate invoiceNr clientName amount viewBtn moreBtn';
     }
   }
   .invoice-row .status {
@@ -81,8 +81,8 @@
   .invoice-row .invoiceNr {
     grid-area: invoiceNr;
   }
-  .invoice-row .client {
-    grid-area: client;
+  .invoice-row .clientName {
+    grid-area: clientName;
   }
   .invoice-row .amount {
     grid-area: amount;
