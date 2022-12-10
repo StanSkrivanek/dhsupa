0d68d734c17085b1fd6b2346786c8302ed864b3d
Stan Skrivanek <s.skrivanek@icloud.com>
2022-12-10 14:15:11 +0000

    Create Alert Component and add to Login

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import Alert from '$lib/components/Alert.svelte';
   let email: string;
   let password: string;
+  let alertMessage = 'success';
 </script>
 
 <h1 class="mb-4 text-[2.5rem] font-black text-goldenFizz">Login</h1>
 <form action="">
+  <Alert message={alertMessage}/>
   <div class="field">
     <label for="email" class="text-goldenFizz ">Email Address</label>
     <input type="text" id="email" name="email" placeholder="Email" required bind:value={email} />
@@ -25,10 +28,14 @@
       bind:value={password}
     />
   </div>
-    <div class="field">
-        <button type="submit" class="mt-6 min-h-[3.5rem] w-full rounded-lg bg-robinEggBlue text-daisyBush p-4 text-xl font-bold hover:shadow-lg transition-shadow duration-200">Login</button>
-        <p class="mt-4 text-center text-sm text-white">
-            <a href="/signup" class="underline hover:no-underline ">Don't have account yet?</a>
-        </p>
-    </div>
+  <div class="field">
+    <button
+      type="submit"
+      class="mt-6 min-h-[3.5rem] w-full rounded-lg bg-robinEggBlue p-4 text-xl font-bold text-daisyBush transition-shadow duration-200 hover:shadow-lg"
+      >Login</button
+    >
+    <p class="mt-4 text-center text-sm text-white">
+      <a href="/signup" class="underline hover:no-underline ">Don't have account yet?</a>
+    </p>
+  </div>
 </form>
