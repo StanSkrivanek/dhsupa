be508e7605407984588e55f4cdfb712d7912a2d7
Stan Skrivanek <s.skrivanek@icloud.com>
2022-12-12 11:09:16 +0000

    ADD: fly transition to invoices/id/+layout

--- src/routes/(dashboard)/invoices/[id]/+layout@.svelte
+++ src/routes/(dashboard)/invoices/[id]/+layout@.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import {fly} from 'svelte/transition';
   import { afterNavigate, goto } from '$app/navigation';
   import ArrowBack from '$lib/components/Icon/ArrowBack.svelte';
   let previousPage: string | undefined = undefined;
@@ -17,7 +18,7 @@
 />
 
 <div class="h-full min-h-screen w-screen bg-whisper pt-16 lg:pt-12 pb-32 print:bg-transparent print:pt-0 print:pb-0">
-  <main class="mx-auto min-h-screen max-w-screen-lg">
+  <main class=" mx-auto min-h-screen max-w-screen-lg" transition:fly={{y:500, duration:250}}>
     <a href={previousPage ? previousPage : '/invoices'} class="fixed left-7 top-7 text-pastelPurple print:hidden"
       ><ArrowBack /></a
     >
